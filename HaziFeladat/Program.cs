c3d9911276dfc1a198f176e5835e9bb1e8a43296
Marci02 <nagymarci.0218@gmail.com>
2022-11-07 20:08:59 +0100

    Add project files.

---
+++ HaziFeladat/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaziFeladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adatok = "10;5;7";
+
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                if (adatok[i] == ';')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(Convert.ToInt32(adatok[i].ToString())*2);
+                }
+            }
+
+            //foreach (var szam in adatok)
+            //{
+            //    if (szam == ';')
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //    else
+            //    {
+            //        Console.Write(szam);
+
+            //    }
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
